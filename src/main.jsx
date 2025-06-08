1d0b64e0df012c27cb3bd7fdafa92540ae7e184b
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-07 23:15:36 -0700

    imported all routes into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import {dash} from './dash.js'
 import { Component } from 'react';
+import {scrum} from './scrum.js'
+import{sprint} from './sprint.js'
+import {proj} from './proj.js'
+
 //implment lazy later
 const router = createBrowserRouter([
   {
