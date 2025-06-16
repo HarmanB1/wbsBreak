d0a9aaa147ef0022030fbb7d5744a0c65aca89d7
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-16 14:35:40 -0700

    added styling to navbar, and tailwind config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
+import './index.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import {Dash} from './dash.jsx'
--- src/nav.jsx
+++ src/nav.jsx
@@ -2,23 +2,25 @@ import { NavLink} from "react-router";
 
 export const Nav = ()=>{
     return (
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/project">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/sprint">Sprint</NavLink>
-        </li>
-        <li>
-          <NavLink to="/wbs">WBS</NavLink>
-        </li>
-        <li>
-          <NavLink to="/scrum">Scrum</NavLink>
-        </li>
-      </ul>
+      <nav className="flex bg-gray-800 dark:bg-gray-900 text-white items-center justify-between p-4">
+        <ul className="flex space-x-6">
+          <li className="hover:text-gray-400">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="hover:text-gray-400">
+            <NavLink to="/project">Projects</NavLink>
+          </li>
+          <li className="hover:text-gray-400">
+            <NavLink to="/sprint">Sprint</NavLink>
+          </li>
+          <li className="hover:text-gray-400">
+            <NavLink to="/wbs">WBS</NavLink>
+          </li>
+          <li className="hover:text-gray-400">
+            <NavLink to="/scrum">Scrum</NavLink>
+          </li>
+        </ul>
+      </nav>
     );
 }
 
