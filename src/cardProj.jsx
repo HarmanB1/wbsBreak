4c15803a199532efd9d829ae13e8ce2d4b8b0b03
Harman Bhogal <bhogalharman7@gmail.com>
2025-06-22 22:42:53 -0700

    added usestate to keep track of options button

--- src/cardProj.jsx
+++ src/cardProj.jsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
+
 export const Card = ({name, description, date, imgSrc })=>{
-    return(
-        <div className="">
-            <h1 className="text-xl font-bold mb-2">{name}</h1>
-            <img src={imgSrc} alt="Card image" />
+
+    const[option, setOption] = setOption(false);
+
+    return (
+      <div>
+        <button onClick={()=> setOption(!option)} > . . .</button>
+        {
+            option &&
             <div>
-                <h2>Description</h2>
-                <p>{description}</p>
-                <p>Date: {date}</p>
-
             </div>
-            
-
-
+        }
+        <div className="">
+          <h1 className="text-xl font-bold mb-2">{name}</h1>
+          <img src={imgSrc} alt="Card image" />
+          <div>
+            <h2>Description</h2>
+            <p>{description}</p>
+            <p>Date: {date}</p>
+          </div>
         </div>
-
+      </div>
     );
 }
