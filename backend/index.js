4103789180dd901eca9b2b7ab20bb06bf86c1645
Harman Bhogal <bhogalharman7@gmail.com>
2025-07-16 15:42:21 -0700

    added, basic index.js implementation for backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    console.log(req);
+});
+
+const PORT = process.env.PORT || 3000; //falbackDev
+app.listen(PORT, ()=>{
+    console.log(`server listening on port ${PORT}`);
+});
