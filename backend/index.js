b4a12a3818b325d94d8d55eeb1e67292bb1e7a84
Harman Bhogal <bhogalharman7@gmail.com>
2025-07-28 22:54:47 -0700

    added connection from project route inot index js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import projectsRouter from "./routes/project.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use('/api/projects', projectsRouter)
 
 
 const PORT = process.env.PORT || 3000; //falbackDev
