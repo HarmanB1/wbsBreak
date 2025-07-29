fbf787f92c7abbca7c726e89e1fed09d72f198f9
Harman Bhogal <bhogalharman7@gmail.com>
2025-07-28 22:53:02 -0700

    added code form index.js into route

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,6 @@ const app = express();
 app.use(cors());
 
 
-app.get('/api/projects', (req, res)=>{
-    res.json("blank");
-});
-
-app.get('/api/projects/:id', (req, res)=>{
-    //query database
-    res.json("blank");
-});
 const PORT = process.env.PORT || 3000; //falbackDev
 
 app.listen(PORT, ()=>{
--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,3 +1,14 @@
 import express from "express";
 const router = express.router();
 
+
+router.get("/", (req, res) => {
+  res.json("blank");
+});
+
+router.get("/:id", (req, res) => {
+  //query database
+  res.json("blank");
+});
+
+export default router;
