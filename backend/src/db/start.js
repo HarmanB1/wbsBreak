c89ffd70ec262c31baf91474357887e12ef3cff7
Harman Bhogal <bhogalharman7@gmail.com>
2025-07-31 21:08:47 -0700

    added, migrations, then changed seperate instances of database connection like username, password etc. to just one url

--- backend/src/db/start.js
+++ backend/src/db/start.js
@@ -6,11 +6,7 @@ dotenv.config();
 const {Pool} = pg;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export const query = async (text, params) =>{
