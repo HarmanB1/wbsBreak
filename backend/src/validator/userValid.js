0f2f90f9edd07ef5089b83dd56563882493f7dc9
Harman Bhogal <bhogalharman7@gmail.com>
2025-08-17 15:56:52 -0700

    added middlware that takes in the sanitized data and checks for errors

--- backend/src/validator/userValid.js
+++ backend/src/validator/userValid.js
@@ -11,3 +11,15 @@ export const regUserValid = [
     .notEmpty().withMessage("Email is required")
     .isLength({min: 6}).withMessage("password must be at least 6 characters")
 ];
+
+export const loginValid = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email must be valid"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Email is required")
+];
