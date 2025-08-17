319e7e8f7771c97441621839c942ee5c1474d3a6
Harman Bhogal <bhogalharman7@gmail.com>
2025-08-16 20:21:57 -0700

    added routes into logged in folder to make easier seperation of files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,20 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import {Dash} from './dash.jsx'
 import { StrictMode } from "react";
-import {Scrum} from './scrum.jsx'
-import{Sprint} from './sprint.jsx'
-import {Proj} from './proj.jsx'
-import { Wbs } from './wbs.jsx';
+import {Scrum} from './loggedIN/scrum.jsx'
+import{Sprint} from './loggedIN/sprint.jsx'
+import {Proj} from './loggedIN/proj.jsx'
+import { Wbs } from './loggedIN/wbs.jsx';
 import { Layout} from './layout.jsx';
-import { Setting } from './setting.jsx';
-import { Profile } from './profile.jsx';
-import { Stats } from './stats.jsx';
+import { Setting } from './loggedIN/setting.jsx';
+import { Profile } from './loggedIN/profile.jsx';
+import { Stats } from './loggedIN/stats.jsx';
+
+import { Landing } from './landingPage/landing.jsx';
+import { SignUp } from './landingPage/signup.jsx';
+import { LogIn } from './landingPage/login.jsx';
+import { Doc } from './landingPage/doc.jsx';
+import { Pricing } from './landingPage/pricing.jsx';
 
 //implment lazy later
 const router = createBrowserRouter([
@@ -21,8 +27,10 @@ const router = createBrowserRouter([
     element: <Landing />,
     children: [
       { path: "login", element: <Login /> },
-      { path: "signup", element: <Signup /> }
-    ]
+      { path: "signup", element: <Signup /> },
+      { path: "signup", element: <Signup /> },
+      { path: "signup", element: <Signup /> },
+    ],
   },
   {
     path: "/app",
@@ -45,12 +53,12 @@ const router = createBrowserRouter([
               { path: "wbs", element: <Wbs /> },
               { path: "sprint", element: <Sprint /> },
               { path: "scrum", element: <Scrum /> },
-            ]
-          }
-        ]
-      }
-    ]
-  }
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
