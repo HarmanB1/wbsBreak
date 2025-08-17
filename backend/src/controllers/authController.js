6529186d42a0c83ae08c866a76b168bd78f577b0
Harman Bhogal <bhogalharman7@gmail.com>
2025-08-17 15:59:25 -0700

    cleaned up controller logic as data checking is being done by middleware

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,10 +4,6 @@ export const registerUser = async(req, res) =>{
     const {email, password} = req.body;
     
       try{
-        if(!email || !password){
-            return res.status(400).json({error: "email or password not sent"});
-        }
-
         if(await getUserByEmail(email)){
             return res.status(409).json({error: "users exists"});
         }
@@ -18,8 +14,7 @@ export const registerUser = async(req, res) =>{
         res.status(201).json({message: "User registered"});
         
       }catch(err){
-        //add later
-        console.log(err);
+        res.status(500).json({error: "Server error"});
       }
 
 }
@@ -28,13 +23,11 @@ export const Login= async(req, res) =>{
   const{email, password} = req.body;
 
   try{
-    if (!email || !password) {
-      return res.status(400).json({ error: "email or password not sent" });
-    }
+   
     
     
     
   }catch(err){
-    console.log(err);
+    res.status(500).json({ error: "Server error" });
   }
 }
