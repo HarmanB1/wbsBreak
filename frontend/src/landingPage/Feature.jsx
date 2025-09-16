81a4ddef1f2608837774fdb7c1233db37575b25f
Harman Bhogal <bhogalharman7@gmail.com>
2025-09-16 15:55:23 -0700

    added, bsaic feature page outlien

--- frontend/src/landingPage/Feature.jsx
+++ frontend/src/landingPage/Feature.jsx
@@ -1,3 +1,26 @@
+import { NavLink } from "react-router";
 export const Features = () => {
-  return <h1>jklsfjsfsfssss skjfskljfkls</h1>;
-};
+  return (
+    <div className="flex flex-col items-center  bg-orange-50 w-full min-h-screen">
+      <h1 className="flex   text-xl">
+        Transfrom your devolper Jounry
+      </h1>
+      <h2>
+        [App Name] helps solo developers plan, organize, and execute projects
+        like a full Agile team. Turn ideas into structured work breakdowns,
+        prioritize tasks, and track progress effortlessly — all while simulating
+        Scrum workflows. Stay focused, work smarter, and move your projects from
+        concept to completion with the guidance of your virtual Scrum Master.
+      </h2>
+      <h3>Timeline</h3>
+      <h1>feature 1</h1>
+
+      <h1>Ready to get started?</h1>
+          <NavLink
+            to="/login"
+            className="border-2 p-4 rounded-lg border-red-300 bg-orange-600 text-white"
+          >
+            Sign Up Now
+          </NavLink>
+    </div>
+  );}
