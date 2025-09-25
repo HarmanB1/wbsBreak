2f24298ad1ef1987ef6fe37f47b1f1b043ca81a1
Harman Bhogal <bhogalharman7@gmail.com>
2025-09-24 23:29:47 -0700

    added, sections page which will have carded sections of each main feature, the cards will explain the sub feature of the main feature

--- frontend/src/landingPage/Feature.jsx
+++ frontend/src/landingPage/Feature.jsx
@@ -1,28 +1,39 @@
-import {Timeline} from "./Timeline.jsx";
+import { Timeline } from "./Timeline.jsx";
 import { NavLink } from "react-router";
 export const Features = () => {
   return (
-    <div className="flex flex-col items-center  bg-orange-50 w-full min-h-screen">
-      <h1 className="flex   text-xl">
-        Transfrom your devolper Jounry
-      </h1>
-      <h2 className="flex items-center px-6 text-center">
-        [App Name] helps solo developers plan, organize, and execute projects
-        like a full Agile team. Turn ideas into structured work breakdowns,
-        prioritize tasks, and track progress effortlessly — all while simulating
-        Scrum workflows. Stay focused, work smarter, and move your projects from
-        concept to completion with the guidance of your virtual Scrum Master.
-      </h2>
-      <h3>Timeline</h3>
-      <Timeline>ss</Timeline>
-      <h1>feature 1</h1>
+    <div className="flex flex-col items-center  bg-orange-50 text-center w-full min-h-screen">
+      <div>
+        <h1 className="flex bold  font-bold text-8xl pt-40">
+          Transfrom your devolper Jounry
+        </h1>
+        <h2 className="flex items-center px-6 text-center ">
+          [App Name] helps solo developers plan, organize, and execute projects
+          like a full Agile team. Turn ideas into structured work breakdowns,
+          prioritize tasks, and track progress effortlessly — all while
+          simulating Scrum workflows. Stay focused, work smarter, and move your
+          projects from concept to completion with the guidance of your virtual
+          Scrum Master.
+        </h2>
+      </div>
+      <div className="pt-4">
+        <h3>Timeline</h3>
+        <Timeline>ss</Timeline>
+      </div>
+      <div>
+        <div>
+
+        </div>
+      </div>
 
       <h1>Ready to get started?</h1>
-          <NavLink
-            to="/login"
-            className="border-2 p-4 rounded-lg border-red-300 bg-orange-600 text-white"
-          >
-            Sign Up Now
-          </NavLink>
+
+      <NavLink
+        to="/login"
+        className="border-2 p-4 rounded-lg border-red-300 bg-orange-600 text-white"
+      >
+        Sign Up Now
+      </NavLink>
     </div>
-  );}
+  );
+};
