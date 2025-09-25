f4e82375a378da0717e5d636a88628741816480f
Harman Bhogal <bhogalharman7@gmail.com>
2025-09-25 00:11:02 -0700

    added, sub section titles

--- frontend/src/landingPage/sections.jsx
+++ frontend/src/landingPage/sections.jsx
@@ -1,22 +1,81 @@
 export const Sections = () => {
-   const section = [
-
-    { name: "Ideas to Tasks", text: "ss", tile: [] },
-    { name: "Ideas to Tasks", text: "ss", tile: [] },
-    { name: "Ideas to Tasks", text: "ss", tile: [] },
+  const section = [
+    {
+      name: "Ideas to Tasks",
+      text: "ss",
+      tile: [
+        { name: "Automatic Breakdown", link: "", text: "" },
+        { name: "Visual Task Tree View", link: "", text: "" },
+        {
+          name: "Tagging, Categorizations, and dependcay breakdown of tasks",
+          link: "",
+          text: "",
+        },
+        {
+          name: "Hierarchal Breakdown",
+          link: "",
+          text: "",
+        },
+        {
+          name: "Progress Tracking",
+          link: "",
+          text: "",
+        },
+      ],
+    },
+    {
+      name: "Agile Enviroment",
+      text: "ss",
+      tile: [
+        { name: "Scrum Board", link: "", text: "" },
+        { name: "Sprint Management", link: "", text: "" },
+        {
+          name: "Task Assignment",
+          link: "",
+          text: "",
+        },
+        {
+          name: "Burndown Charts",
+          link: "",
+          text: "",
+        },{
+          name: "Scrum Master",
+          link: "",
+          text: "",
+        }
+        ,
+      ],
+    },
+    {
+      name: "Scheduling & Time Management",
+      text: "ss",
+      tile: [
+        { name: "Task Deadlines", link: "", text: "" },
+        { name: "Calendar View", link: "", text: "" },
+        {
+          name: "Notification & Reminders",
+          link: "",
+          text: "",
+        },
+        {
+          name: "Time Estimates",
+          link: "",
+          text: "",
+        },
+        ,
+      ],
+    },
   ];
 
   return (
     <div>
       {section.map((block, index) => (
         <div className="flex flex-col" key={index}>
-            <h1 className="text-4xl">{block.name}</h1>
-            <div className=""> {block.text}</div>
+          <h1 className="text-4xl">{block.name}</h1>
+          <div className=""> {block.text}</div>
 
           {block.tile.map((card, indexer) => (
-            <div key={indexer} className="grid grid-cols-3 ">
-
-            </div>
+            <div key={indexer} className="grid grid-cols-3 "></div>
           ))}
         </div>
       ))}
