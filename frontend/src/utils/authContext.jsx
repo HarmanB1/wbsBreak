55ff2de0f23d315fcdb5bf463622db615634faef
Harman Bhogal <bhogalharman7@gmail.com>
2025-09-28 21:21:14 -0700

    added dev login

--- frontend/src/utils/authContext.jsx
+++ frontend/src/utils/authContext.jsx
@@ -4,11 +4,31 @@ import { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserAuth = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(()=>{
+    const savedUser = localStorage.getItem("user");
+    if(!savedUser) return null;
 
-  const login = (userData) => setUser(userData);
-  const logout = () => setUser(null);
+    try{
+        return JSON.parse(savedUser);
+}catch{
+    console.warn("invalid");
+    localStorage.removeItem("user");
+    return null;
+}
+  });
 
+  const login = (userData) => {setUser(userData); localStorage.setItem("user", JSON.stringify(userData));}
+  const logout = () => {  setUser(null); localStorage.removeItem("user");}
+
+ if(typeof window !== "undefined") {
+    window.loginAs = () => login({ name: "dev" });
+
+    window.logout = logout;
+    window.userIs = () => {
+        console.log(JSON.parse(localStorage.getItem("user")));
+    }
+ }
+  
   return (
     <UserContext.Provider value={{ user, login, logout }}>
       {children}
