61d16da223e4cc70e36134105dde67dca1532c94
Harman Bhogal <bhogalharman7@gmail.com>
2025-10-22 19:42:44 -0700

    added, active link stylilng

--- frontend/src/pubLayout/pubNavBar.jsx
+++ frontend/src/pubLayout/pubNavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 export const PubNavBar = () => {
   const navItems = [
     { name: "home", link: "/" },
@@ -7,12 +8,13 @@ export const PubNavBar = () => {
   ];
 
   return (
-    <nav className="fixed p-8 text-3xl z-50">
+    <nav className="fixed p-8 text-2xl z-50">
       <div className="flex space-x-4 w-fit border-2 border-black shadow-2xl items-center bg-gray-100 m-0 rounded-full p-3 backdrop-blur-md bg-opacity-50">
         <div>LOGO</div>
         <ul className="flex space-x-4">
           {navItems.map((link) => (
-            <Link key={link.name} to= {link.link}>{link.name}</Link>
+            <NavLink className={({ isActive }) => isActive ? "text-blue-500 font-bold" : "text-black"}
+             key={link.name} to= {link.link}>{link.name}</NavLink>
           ))}
         </ul>
       </div>
