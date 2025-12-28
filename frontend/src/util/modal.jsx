2b3451df5271fdf8a9c95f322bc8d425b6268ff8
Harman Bhogal <bhogalharman7@gmail.com>
2025-12-27 20:43:17 -0800

    added modal component

--- frontend/src/util/modal.jsx
+++ frontend/src/util/modal.jsx
@@ -1,29 +1,40 @@
 import { motion, AnimatePresence } from 'framer-motion'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 export const Modal = ({ open, anchorEl, setOpen, children }) => {
   const [pos, setPos] = useState(null);
   //handle pos
   useEffect(() => {
-
-    const elemPos = anchorEl.current.getBoundingClientReact();
-    setPos({ top: elemPos.top, left: elemPos.left });
-
+    if (anchorEl.current && open) {
+      const elemPos = anchorEl.current.getBoundingClientReact();
+      setPos({ top: elemPos.top, left: elemPos.left });
+    }
     const handleClick = (e) => {
       if (open && anchorEl.current && anchorEl.contains(e.target)) {
         setOpen(false);
       }
     }
 
-  }, [open, anchorEl]);
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    }
+
+  }, [open, anchorEl, setOpen]);
 
   return (
     < AnimatePresence >
       {open &&
         (
           <motion.div
-            initial={{}}
+            initial={{ opacity: 0, scale: 0.90 }}
+            animate={{ opacity: 1, scale: 1 }}
+            style={{
+              top: pos.top,
+              left: pos.left
+            }}
+            className="bg-slate-600"
 
           >
             {children}
