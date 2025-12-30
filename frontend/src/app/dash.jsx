f2b376959bf4987afd80e3b96a88aaac9141a3a9
Harman Bhogal <bhogalharman7@gmail.com>
2025-12-29 21:57:13 -0800

    added, basic jump back in section in dash

--- frontend/src/app/dash.jsx
+++ frontend/src/app/dash.jsx
@@ -1,14 +1,54 @@
+import { Link } from "react-router-dom"
+
+{//temp data
+}
+const weekData = {
+    mon: [
+        { id: 1, title: 'Team Meeting', start: '10:00', end: '12:00', description: 'Weekly sync' },
+        { id: 2, title: 'Lunch', start: '12:00', end: '13:00' }
+    ],
+    tue: [
+        { id: 3, title: 'Code Review', start: '14:00', end: '15:30' }
+    ],
+    wed: [],
+    thu: [
+        { id: 4, title: 'Client Call', start: '9:00', end: '10:00' }
+    ],
+    fri: [],
+    sat: [],
+    sun: []
+};
+
 export const Dash = () => {
     return (
-        <div>
-            <div>
-                Jump Back In
+        <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-4">
+                <h1 className="text-5xl text-black  font-bold">Jump Back In</h1>
+                <div className="border border-gray-700 w-32 p-3 rounded-lg ">
+                    <Link to="/app/projects">
 
+                        thing replaced by api call to cache
+
+                    </Link>
+                </div>
             </div>
-            {//as clanendar thing
-            }
+
             <div>
-                Priority schedule: list of tasks assigned
+                {//api call which will send weeks task data back. specfic endpoint
+                }
+                <h1 className="text-5xl text-black  font-bold">Schedule</h1>
+                <div className="border border-gray-700 w-32 p-3 rounded-lg ">
+                    {//main schedule calender
+                    }
+                    <div className="overflow-x-auto"
+
+                    >
+                        <div className="grid grid-cols-2 gap-2">
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <div>
                 Activity feed
