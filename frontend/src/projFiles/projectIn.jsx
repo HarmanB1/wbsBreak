1a9f7fd10999af63f413a652f1edefe645f2ea16
Harman Bhogal <bhogalharman7@gmail.com>
2026-01-05 18:21:45 -0800

    added floating nav bar menu

--- frontend/src/projFiles/projectIn.jsx
+++ frontend/src/projFiles/projectIn.jsx
@@ -1,16 +1,20 @@
 import { Outlet, useParams } from "react-router-dom"
 import { ProjectInNav } from "./projectInNav";
+import { useState } from "react";
 export const ProjectIn = () => {
+    const [open, setOpen] = useState(false);
     const { projectId } = useParams();
     return (
-        <div>
+        <div className="relative flex flex-col">
 
             <h2>{projectId}</h2>
             sfsfs
             <h2 className="inline-block"> PROJECT TITLE</h2>
-
-            <ProjectInNav />
-            <Outlet />
+            <button className="cursor-pointer hover:bg-blue-100 " onClick={() => { setOpen(true) }}>BUTTOn</button>
+            <div className="absolute  z-10">
+                <ProjectInNav open={open} setOpen={setOpen} />
+                <Outlet />
+            </div>
         </div>
 
     );
